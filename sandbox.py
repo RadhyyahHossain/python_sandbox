00ae67b8d20afa0d4d2218cfb0b58226cdc7ebf7
Radhyyah Hossain <radiabxd@gmail.com>
2021-12-04 00:11:22 -0500

    practice code

--- sandbox.py
+++ sandbox.py
@@ -1,1 +1,32 @@
-print("whats up")
+#User interaction
+#name = input("Enter your name: ")
+#age = input("Enter your age: ")
+#print("Hello " + name + "! You are " + age)
+
+
+#Basic calculator
+#num1 = input("Enter a number: ")
+#num2 = input("Enter another number: ")
+#result = float(num1) + float(num2)
+#print (result)
+
+
+#Mad Libs Game
+#food = input("Enter a food: ")
+#object = input("Enter an object: ")
+#senses = input("Enter a sense: ")
+#place = input("Enter a place: ")
+#punishment = input("Enter a punishment: ")
+
+#print("We went out to eat " + food)
+#print("Min bought " + object)
+#print("It " + senses + " like dragon ball Z kai")
+#print("So he threw it out in the " + place)
+#print("There were cameras so he got " + punishment)
+
+#Lists
+
+#friends = ["Bill", "Bob", "Tess", "Jim"]
+#friends[1] = "Mike"
+
+#print(friends)
